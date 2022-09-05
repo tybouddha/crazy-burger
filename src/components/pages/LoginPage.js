ca71696a1e064334b8546be841a2fafd9d15c2ac
Vi Dev <vi@videv.fr>
2022-09-05 17:44:36 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,3 +1,36 @@
+import { useState } from "react"
+
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  // state
+  const [inputValue, setInputValue] = useState("")
+
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  // affichage (render)
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entre votre prénom..."
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  )
 }
