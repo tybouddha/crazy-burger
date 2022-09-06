cde31a70b8140314c8e799ac70b1bf396336f58f
Vi Dev <vi@videv.fr>
2022-09-06 13:12:14 +0200

    ✨ F02: LOGIN > add redirection to ErrorPage on Submit with username

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function LoginForm() {
   // state
   const [inputValue, setInputValue] = useState("")
+  const navigate = useNavigate()
 
   // comportements
   const handleSubmit = (event) => {
     event.preventDefault()
-    alert(`Bonjour ${inputValue}`)
     setInputValue("")
+    navigate(`order/${inputValue}`)
   }
 
   const handleChange = (event) => {
@@ -30,7 +31,6 @@ export default function LoginForm() {
         required
       />
       <button>Accédez à votre espace</button>
-      <Link to="/order">Vers OrderPage</Link>
     </form>
   )
 }
