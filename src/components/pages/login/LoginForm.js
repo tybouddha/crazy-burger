17ec7e0df7dbe53a9da7dc7e8644643a3316ee33
Vi Dev <vi@videv.fr>
2022-09-06 12:24:53 +0200

    ✨ F02: LOGIN > add redirection from/to OrderPage

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function LoginForm() {
   // state
@@ -29,6 +30,7 @@ export default function LoginForm() {
         required
       />
       <button>Accédez à votre espace</button>
+      <Link to="/order">Vers OrderPage</Link>
     </form>
   )
 }
