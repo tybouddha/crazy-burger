54f564008b8bc8b34b8bb375fded62ba4d95d3fa
Vi Dev <vi@videv.fr>
2022-11-10 20:05:27 +0100

    💄 F03-live-AMA : 4 méthodes pour ajouter du style

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import "./LoginForm.css"
 
 export default function LoginForm() {
   // state
@@ -17,12 +18,21 @@ export default function LoginForm() {
     setInputValue(event.target.value)
   }
 
+  const fruit = {
+    nom: "abricot",
+    origin: "France",
+  }
+
+  const titreH2Style = { backgroundColor: "green", color: "white", fontSize: 20 }
+
   // affichage
   return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
+      <h1 style={{ color: "red", backgroundColor: "green" }} className="alex">
+        Bienvenue chez nous !
+      </h1>
       <br />
-      <h2>Connectez-vous</h2>
+      <h2 className="bonbon">Connectez-vous</h2>
       <input
         value={inputValue}
         onChange={handleChange}
@@ -34,3 +44,15 @@ export default function LoginForm() {
     </form>
   )
 }
+
+/** 4 méthodes pour ajouter du style à un composant
+ * 1. inline style
+ * 2. object style
+ * 3. modules CSS (avec className)
+ * 4. global style (index.css)
+ */
+
+/** Styled-Components
+ * ✅ éviter le style collision (CSS, Sass)
+ * ✅ conserver nested styling (Sass)
+ */
