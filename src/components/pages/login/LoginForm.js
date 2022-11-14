1a6affd0ed68144772be87c8e49895ed25ee772d
Vi Dev <vi@videv.fr>
2022-11-14 21:29:30 +0100

    💄 F03: LOGIN > LoginForm > button > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import { BsPersonCircle } from "react-icons/bs"
-import { theme } from "../../../theme"
+import { IoChevronForward } from "react-icons/io5"
 
 export default function LoginForm() {
   // state
@@ -39,14 +39,17 @@ export default function LoginForm() {
             required
           />
         </div>
-        <button>Accéder à mon espace</button>
+
+        <button className="button-with-icon">
+          <span>Accéder à mon espace</span>
+          <IoChevronForward className="icon" />
+        </button>
       </div>
     </LoginFormStyled>
   )
 }
 
 const LoginFormStyled = styled.div`
-  background: green;
   text-align: center;
   max-width: 500px;
   min-width: 400px;
@@ -100,6 +103,52 @@ const LoginFormStyled = styled.div`
     &::placeholder {
       background: white;
       color: lightgrey;
+    }
+  }
+
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative; //is used in case you want to create interactive icons where an icon replaces the text label.
+    white-space: nowrap; //prevents the text label from wrapping to the next line.
+    text-decoration: none; //removes the text decoration in case you’re applying the .btn class to a link.
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
     }
   }
 `
