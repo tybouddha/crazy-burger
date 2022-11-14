13ebc697eafb0aab5fed85efe2b783ce64a70250
Vi Dev <vi@videv.fr>
2022-11-14 20:24:28 +0100

    🔥 F03 (live ama) : LoginForm > reset LoginForm before cours Styled Components

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import { theme } from "../../../theme"
 
@@ -19,78 +19,24 @@ export default function LoginForm() {
     setInputValue(event.target.value)
   }
 
-  const fruit = {
-    nom: "abricot",
-    origin: "France",
-  }
-
-  const titreH2Style = { backgroundColor: "green", color: "white", fontSize: 20 }
-
   // affichage
   return (
-    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <div className="container-text">
-        <h1 className="alex">Bienvenue chez nous !</h1>
+    <div action="submit" onSubmit={handleSubmit}>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
         <br />
-        <h2 className="bonbon">Connectez-vous</h2>
+        <h2>Connectez-vous</h2>
       </div>
-      <div className="cta-container">
+      <div>
         <input
           value={inputValue}
           onChange={handleChange}
           type="text"
-          placeholder="Entre votre prénom..."
+          placeholder="Entrez votre prénom"
           required
         />
-        <button>Accédez à votre espace</button>
+        <button>Accéder à mon espace</button>
       </div>
-    </LoginFormStyled>
+    </div>
   )
 }
-
-const LoginFormStyled = styled.form`
-  border: 1px solid red;
-
-  .container-text {
-    background: grey;
-    border: 1px solid ${theme.colors.tiertiary};
-
-    h1 {
-      border: 1px solid red;
-      color: ${theme.colors.primary};
-    }
-
-    h2 {
-      color: ${theme.colors.tiertiary};
-      border: 1px solid blue;
-    }
-  }
-
-  .cta-container {
-    background: ${theme.colors.green};
-    height: 500px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    input {
-      border: 1px solid yellow;
-    }
-
-    button {
-      border: 1px solid purple;
-    }
-  }
-`
-
-/** 4 méthodes pour ajouter du style à un composant
- * 1. inline style
- * 2. object style
- * 3. modules CSS (avec className)
- * 4. global style (index.css)
- */
-
-/** Styled-Components
- * ✅ éviter le style collision (CSS, Sass)
- * ✅ conserver nested styling (Sass)
- */
