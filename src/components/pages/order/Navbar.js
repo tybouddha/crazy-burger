e8c9a438f9b87f680c67e367753dd3779c312ea5
Vi Dev <vi@videv.fr>
2022-11-27 21:33:22 +0100

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -2,11 +2,12 @@ import styled from "styled-components"
 import NavbarRightSide from "./NavbarRightSide"
 import Logo from "../../reusable-ui/Logo"
 import { theme } from "../../../theme"
+import { refreshPage } from "../../../utils/window"
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <Logo />
+      <Logo className="logo-order-page" onClick={() => refreshPage()} />
       <NavbarRightSide username={username} />
     </NavbarStyled>
   )
@@ -22,4 +23,8 @@ const NavbarStyled = styled.nav`
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
   /* align-items: center; */
+
+  .logo-order-page {
+    cursor: pointer;
+  }
 `
