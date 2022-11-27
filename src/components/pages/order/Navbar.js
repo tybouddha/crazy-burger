97cd598146711c5abbacbda3bdd4a5d2052502a2
Vi Dev <vi@videv.fr>
2022-11-27 20:28:05 +0100

    🚧 F04: ORDER > layout > navbar > add left-side and right-side

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -4,11 +4,14 @@ import styled from "styled-components"
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      Navbar
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+      <div className="left-side">Left</div>
+      <div className="right-side">
+        Right
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
     </NavbarStyled>
   )
 }
@@ -16,4 +19,14 @@ export default function Navbar({ username }) {
 const NavbarStyled = styled.nav`
   background: blue;
   height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+
+  .left-side {
+    background: pink;
+  }
+  .right-side {
+    background: purple;
+  }
 `
