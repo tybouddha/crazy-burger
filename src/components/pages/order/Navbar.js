251956948935f6202fd146a4249dfec90e8b218a
Vi Dev <vi@videv.fr>
2022-11-27 20:49:40 +0100

    ✨ F04: ORDER > Navbar > Add Logo

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -1,10 +1,11 @@
 import styled from "styled-components"
 import NavbarRightSide from "./NavbarRightSide"
+import Logo from "../../reusable-ui/Logo"
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <div className="left-side">Left</div>
+      <Logo />
       <NavbarRightSide username={username} />
     </NavbarStyled>
   )
@@ -15,9 +16,6 @@ const NavbarStyled = styled.nav`
   height: 10vh;
   display: flex;
   justify-content: space-between;
+  padding: 0 20px;
   /* align-items: center; */
-
-  .left-side {
-    background: pink;
-  }
 `
