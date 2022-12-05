3e290e49d0506384648ebab05d9fc1dc3e590cc1
Vi Dev <vi@videv.fr>
2022-12-05 20:20:45 +0100

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -2,7 +2,12 @@ import styled from "styled-components"
 import { theme } from "../../../theme"
 
 export default function Main() {
-  return <MainStyled className="main"></MainStyled>
+  return (
+    <MainStyled className="main">
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  )
 }
 
 const MainStyled = styled.div`
@@ -12,4 +17,15 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
+  .basket {
+    background: pink;
+  }
+
+  .menu {
+    background: purple;
+  }
 `
