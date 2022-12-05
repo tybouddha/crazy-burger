64a9b1be43878149f70f89f63fef5d12b95a4e76
Vi Dev <vi@videv.fr>
2022-12-05 22:41:34 +0100

    ✨ F05: ORDER > Product > Add formatted price

--- src/components/pages/order/Main/Product.js
+++ src/components/pages/order/Main/Product.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { theme } from "../../../../theme"
 import PrimaryButton from "../../../reusable-ui/PrimaryButton"
+import { formatPrice } from "../../../../utils/maths"
 
 export default function Product({ title, imageSource, price }) {
   return (
@@ -11,7 +12,7 @@ export default function Product({ title, imageSource, price }) {
       <div className="text-info">
         <div className="title">{title}</div>
         <div className="description">
-          <div className="left-description">{price}</div>
+          <div className="left-description">{formatPrice(price)}</div>
           <div className="right-description">
             <PrimaryButton className="primary-button" label={"Ajouter"} />
           </div>
