1b6e97288a3aa9ac8add23d7b5281f8fbeba43eb
Vi Dev <vi@videv.fr>
2022-12-05 21:21:03 +0100

    ♻️ F05: ORDER > MENU > Create <Product/>

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import styled from "styled-components"
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
 import { theme } from "../../../../theme"
+import Product from "./Product"
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2)
@@ -10,18 +11,7 @@ export default function Menu() {
     <MenuStyled className="menu">
       {menu.map((produit) => {
         return (
-          <div className="produit">
-            <div className="image">
-              <img src={produit.imageSource} alt={produit.title} />
-            </div>
-            <div className="info-text">
-              <div className="title">{produit.title}</div>
-              <div className="description">
-                <div className="price">{produit.price}</div>
-                <button className="add-button">Ajouter</button>
-              </div>
-            </div>
-          </div>
+          <Product title={produit.title} imageSource={produit.imageSource} price={produit.price} />
         )
       })}
     </MenuStyled>
@@ -36,20 +26,4 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-  .produit {
-    background: red;
-    width: 240px;
-    height: 330px;
-
-    .image {
-      border: 1px solid fuchsia;
-      width: 100px;
-      height: auto;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
 `
