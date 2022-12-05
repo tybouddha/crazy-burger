823c6cfa2e820b64cbfc11d8613c3fb0a1324f1c
Vi Dev <vi@videv.fr>
2022-12-05 22:08:03 +0100

    💄 F05: ORDER > Product > Add image layout

--- src/components/pages/order/Main/Product.js
+++ src/components/pages/order/Main/Product.js
@@ -7,8 +7,8 @@ export default function Product({ title, imageSource, price }) {
         <img src={imageSource} alt={title} />
       </div>
       <div className="info-text">
+        <div className="title">{title}</div>
         <div className="description">
-          <div className="title">{title}</div>
           <div className="price">{price}</div>
           <button className="add-button">Ajouter</button>
         </div>
@@ -19,22 +19,27 @@ export default function Product({ title, imageSource, price }) {
 
 const ProductStyled = styled.div`
   background: red;
-  width: 240px;
-  height: 330px;
+  width: 200px;
+  height: 300px;
+  display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  padding-bottom: 10px;
 
   .image {
-    border: 1px solid fuchsia;
-    width: 100px;
+    border: 1px solid yellow;
+    width: 100%;
     height: auto;
+    margin-top: 30px;
 
     img {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
     }
   }
 
-  .description {
-    border: 1px solid fuchsia;
+  .info-text {
+    border: 1px solid blue;
   }
 `
