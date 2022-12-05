db3335be6da1edff53bb9decfd3bbe071cca2f4c
Vi Dev <vi@videv.fr>
2022-12-05 20:34:23 +0100

    ♻️ F05: ORDER > MAIN > Create Menu

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,11 +1,12 @@
 import styled from "styled-components"
 import { theme } from "../../../../theme"
+import Menu from "./Menu"
 
 export default function Main() {
   return (
     <MainStyled className="main">
       <div className="basket">Basket</div>
-      <div className="menu">Menu</div>
+      <Menu />
     </MainStyled>
   )
 }
@@ -23,9 +24,5 @@ const MainStyled = styled.div`
 
   .basket {
     background: pink;
-  }
-
-  .menu {
-    background: purple;
   }
 `
---
+++ src/components/pages/order/Main/Menu.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default function Menu() {
+  return <MenuStyled className="menu">Menu</MenuStyled>
+}
+
+const MenuStyled = styled.div`
+  background: purple;
+`
