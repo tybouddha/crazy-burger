5a20beb3b702b11867e7185e26274ff9f5d5241f
Vi Dev <vi@videv.fr>
2022-12-05 20:59:23 +0100

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import styled from "styled-components"
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
+import { theme } from "../../../../theme"
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2)
@@ -15,12 +16,13 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .produit {
     background: red;
