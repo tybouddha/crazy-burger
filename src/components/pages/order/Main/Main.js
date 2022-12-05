455130a8c8bf7fadedda7c9d885c1c6fbf868b6c
Vi Dev <vi@videv.fr>
2022-12-05 20:39:41 +0100

    💡 F05: ORDER > MAIN > Prepare code for <Basket/>

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -5,7 +5,7 @@ import Menu from "./Menu"
 export default function Main() {
   return (
     <MainStyled className="main">
-      <div className="basket">Basket</div>
+      {/* <div className="basket">Basket</div> */}
       <Menu />
     </MainStyled>
   )
@@ -20,9 +20,9 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 25% 1fr;
+  grid-template-columns: 1fr;
 
-  .basket {
+  /* .basket {
     background: pink;
-  }
+  } */
 `
