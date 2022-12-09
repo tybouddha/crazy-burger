fd4c93ced5b5f86f4a61e2b408938831a59875a6
Vi Dev <vi@videv.fr>
2022-12-09 20:42:55 +0100

    💄 F06: NAVBAR > Right side > Add spacing

--- src/components/pages/order/Navbar/NavbarRightSide.js
+++ src/components/pages/order/Navbar/NavbarRightSide.js
@@ -1,10 +1,14 @@
 import styled from "styled-components"
 import Profile from "./Profile"
+import ToggleButton from "../../../reusable-ui/ToggleButton"
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled className="right-side">
-      {/* <div className="admin-button">Admin Button</div> */}
+      <ToggleButton
+        labelIfUnchecked="ACTIVER LE MODE ADMIN"
+        labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+      />
       <Profile username={username} />
     </NavbarRightSideStyled>
   )
@@ -14,12 +18,4 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
-
-  /* .admin-button {
-    background: lightblue;
-  } */
-
-  .profile {
-    background: yellow;
-  }
 `
