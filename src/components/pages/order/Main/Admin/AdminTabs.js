92222178bf2eccff7fc19d48c718790ee4a2c648
Vi Dev <vi@videv.fr>
2023-02-09 21:37:31 +0100

    ♻️ F07: ADMIN > Tabs > Create Tab

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -1,5 +1,17 @@
 import React from "react"
+import styled from "styled-components"
+import Tab from "../../../../reusable-ui/Tab"
+import { FiChevronDown, FiChevronUp } from "react-icons/fi"
 
 export default function AdminTabs() {
-  return <div>AdminTabs</div>
+  return (
+    <AdminTabsStyled>
+      <Tab Icon={<FiChevronDown />} />
+    </AdminTabsStyled>
+  )
 }
+
+const AdminTabsStyled = styled.div`
+  /* border: 2px solid red; */
+  display: flex;
+`
---
+++ src/components/reusable-ui/Tab.js
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+import { theme } from "../../theme"
+
+export default function Tab({ Icon }) {
+  return (
+    <TabStyled className="tab">
+      <div className="icon">{Icon}</div>
+    </TabStyled>
+  )
+}
+
+const TabStyled = styled.button`
+  border: 1px solid blue;
+  padding: 0 22px;
+  height: 43px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  font-size: ${theme.fonts.size.P0};
+  color: ${theme.colors.greySemiDark};
+
+  position: relative;
+  left: 5%;
+
+  background: #ffffff;
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: #e4e5e9;
+  box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+  border-radius: 5px 5px 0px 0px;
+
+  .icon {
+    display: flex;
+  }
+`
