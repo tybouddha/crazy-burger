c988449ce3bf18c350764d9f66509400dc1907fc
Vi Dev <vi@videv.fr>
2023-02-10 21:00:07 +0100

    🚧 F07: ADMIN > Tabs > add tabsConfig file

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -2,64 +2,37 @@ import React, { useContext } from "react"
 import styled from "styled-components"
 import Tab from "../../../../reusable-ui/Tab"
 import { FiChevronDown, FiChevronUp } from "react-icons/fi"
-import { AiOutlinePlus } from "react-icons/ai"
-import { MdModeEditOutline } from "react-icons/md"
 import { theme } from "../../../../../theme"
 import OrderContext from "../../../../../context/OrderContext"
+import { getTabsConfig } from "./getTabsConfig"
 
 export default function AdminTabs() {
-  const {
-    isCollapsed,
-    setIsCollapsed,
-    isAddSelected,
-    setIsAddSelected,
-    isEditSelected,
-    setIsEditSelected,
-  } = useContext(OrderContext)
+  const { isCollapsed, setIsCollapsed, currentTabSelected, setCurrentTabSelected } =
+    useContext(OrderContext)
 
   const selectTab = (tabSelected) => {
-    setIsCollapsed(false)
-
-    if (tabSelected === "add") {
-      setIsAddSelected(true)
-      setIsEditSelected(false)
-    }
-
-    if (tabSelected === "edit") {
-      setIsEditSelected(true)
-      setIsAddSelected(false)
-    }
+    setIsCollapsed(false) // ouvre moi le panel dans tous les cas
+    setCurrentTabSelected(tabSelected) // réactualise l'onglet sélectionné
   }
 
-  const tabsConfig = [
-    {
-      label: "",
-      Icon: isCollapsed ? <FiChevronUp /> : <FiChevronDown />,
-      onClick: () => setIsCollapsed(!isCollapsed),
-      className: isCollapsed ? "is-active" : "",
-    },
-    {
-      label: "Ajouter un produit",
-      Icon: <AiOutlinePlus />,
-      onClick: () => selectTab("add"),
-      className: isAddSelected ? "is-active" : "",
-    },
-    {
-      label: "Modifier un produit",
-      Icon: <MdModeEditOutline />,
-      onClick: () => selectTab("edit"),
-      className: isEditSelected ? "is-active" : "",
-    },
-  ]
+  const tabs = getTabsConfig(currentTabSelected)
 
   // affichage
   return (
     <AdminTabsStyled>
-      {tabsConfig.map((tab) => {
-        return (
-          <Tab label={tab.label} Icon={tab.Icon} onClick={tab.onClick} className={tab.className} />
-        )
-      })}
+      <Tab
+        Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      {tabs.map((tab) => (
+        <Tab
+          label={tab.label}
+          Icon={tab.Icon}
+          onClick={() => selectTab(tab.index)}
+          className={tab.className}
+        />
+      ))}
     </AdminTabsStyled>
   )
 }
