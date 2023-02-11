f248d6e0b7a56ad82172a2f37901232520a530cc
Vi Dev <vi@videv.fr>
2023-02-11 03:29:14 +0100

    🐛 F07: ORDER > Set isModeAdmin back to false (default value)

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -9,7 +9,7 @@ import OrderContext from "../../../context/OrderContext"
 export default function OrderPage() {
   // state
   const { username } = useParams()
-  const [isModeAdmin, setIsModeAdmin] = useState(true)
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(false)
   const [isAddSelected, setIsAddSelected] = useState(true)
   const [isEditSelected, setIsEditSelected] = useState(false)
