3bc78f90f42cd3f905b7f111035f763d23ea0231
Tybouddha <bruneecedric@gmail.com>
2025-07-16 13:35:15 +0200

    🍻 F07: ALL > clean code

---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import styled from "styled-components"
+import { theme } from "../../../theme"
+import Main from "./Main/Main"
+import Navbar from "./Navbar/Navbar"
+import OrderContext from "../../../context/OrderContext"
+
+export default function OrderPage() {
+  // state
+  const { username } = useParams()
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [currentTabSelected, setCurrentTabSelected] = useState("add")
+
+  // comportements
+
+  const orderContextValue = {
+    isModeAdmin,
+    setIsModeAdmin,
+    isCollapsed,
+    setIsCollapsed,
+    currentTabSelected,
+    setCurrentTabSelected,
+  }
+
+  //affichage
+  return (
+    <OrderContext.Provider value={orderContextValue}>
+      <OrderPageStyled>
+        <div className="container">
+          <Navbar username={username} />
+          <Main />
+        </div>
+      </OrderPageStyled>
+    </OrderContext.Provider>
+  )
+}
+
+const OrderPageStyled = styled.div`
+  background: ${theme.colors.primary};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction: column;
+    border-radius: ${theme.borderRadius.extraRound};
+  }
+`
