c5a256a17198e7b819187c74d837444137b35b42
Tybouddha <bruneecedric@gmail.com>
2025-07-31 08:07:10 +0200

    💄 F08: MENU > Add style on EmptyMenuAdmin and EmptyMenuClient

--- src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
@@ -3,6 +3,7 @@ import { fakeMenu } from "../../../../../fakeData/fakeMenu";
 import { OrderContext } from "../../../../../context/OrderContext";
 import styled from "styled-components";
 import PrimaryButton from "../../../../reusable-ui/PrimaryButton";
+import { theme } from "../../../../../theme";
 
 export default function EmptyMenuAdmin() {
   //state
@@ -16,15 +17,48 @@ export default function EmptyMenuAdmin() {
   //affichage
   return (
     <EmptyMenuAdminStyled>
-      <span>LE MENU EST VIDE?</span>
-      <br />
-      <span>CLIQUEZ CI-DESSOUS POUR LE REINITIALISER </span>
+      <span className="title">Le menu est vide ?</span>
+      <span className="description">
+        Cliquez ci-dessous pour le réinitialiser
+      </span>
       <PrimaryButton
         label={"Générer de nouveaux produits"}
         onClick={resetMenu}
+        className={"reset-button"}
       />
     </EmptyMenuAdminStyled>
   );
 }
 
-const EmptyMenuAdminStyled = styled("div")``;
+const EmptyMenuAdminStyled = styled("div")`
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title,
+  .description {
+    text-align: center;
+    font-family: "Amatic SC", cursive;
+    color: ${theme.colors.greyBlue};
+  }
+
+  .title {
+    font-size: ${theme.fonts.size.P4};
+    font-weight: ${theme.fonts.weights.semiBold};
+  }
+
+  .description {
+    font-size: ${theme.fonts.size.P4};
+    margin-top: 20px;
+  }
+
+  .reset-button {
+    width: auto;
+    margin-top: 30px;
+    font-size: ${theme.fonts.size.XS};
+  }
+`;
--- src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 export default function EmptyMenuClient() {
   return (
     <EmptyMenuClientStyled>
-      <span>VICTIME DE NOTRE SUCCES ! :D</span>
-      <span>DE NOUVELLES RECETTES SONT EN COURS DE PREPARATION.</span>
-      <span>A TRES VITE !</span>
+      <span className="title">VICTIME DE NOTRE SUCCES ! :D</span>
+      <span className="description">
+        DE NOUVELLES RECETTES SONT EN COURS DE PREPARATION.
+      </span>
+      <span className="description">A TRES VITE !</span>
     </EmptyMenuClientStyled>
   );
 }
 
-const EmptyMenuClientStyled = styled("div")``;
+const EmptyMenuClientStyled = styled("div")`
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title,
+  .description {
+    text-align: center;
+    font-family: "Amatic SC", cursive;
+    color: ${theme.colors.greyBlue};
+  }
+
+  .title {
+    font-size: ${theme.fonts.size.P4};
+    font-weight: ${theme.fonts.weights.semiBold};
+  }
+
+  .description {
+    font-size: ${theme.fonts.size.P4};
+    margin-top: 20px;
+  }
+`;
