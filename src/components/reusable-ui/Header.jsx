fc07a8aee8b84df176914077792e13fd0685f908
Tybouddha <bruneecedric@gmail.com>
2025-08-25 18:37:01 +0200

    ♻️ F10: BASKET> reusable Create Header.js

---
+++ src/components/reusable-ui/Header.jsx
@@ -0,0 +1,11 @@
+import { theme } from "../../theme";
+import styled from "styled-components";
+
+export default function Header({ children }) {
+  return <HeaderStyled>{children}</HeaderStyled>;
+}
+
+const HeaderStyled = styled("div")`
+  height: 70px;
+  background: ${theme.colors.background_dark};
+`;
