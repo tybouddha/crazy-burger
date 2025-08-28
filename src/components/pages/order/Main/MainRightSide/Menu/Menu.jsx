0b4b22f3ac9ac8f918f763cbf3facd5cb7fa6c3e
Tybouddha <bruneecedric@gmail.com>
2025-08-28 15:35:28 +0200

    ✨ F11: MENU add handleDeleteBasketProduct to handleOnDelete

--- src/components/pages/order/Main/MainRightSide/Menu/Menu.jsx
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.jsx
@@ -25,6 +25,7 @@ export default function Menu() {
     setCurrentTabSelected,
     titleEditRef,
     handleAddToBasket,
+    handleDeleteBasketProduct,
   } = useContext(OrderContext);
   // state
 
@@ -42,6 +43,7 @@ export default function Menu() {
   const handleCardDelete = (event, idProductToDelete) => {
     event.stopPropagation();
     handleDelete(idProductToDelete);
+    handleDeleteBasketProduct(idProductToDelete);
     idProductToDelete === productSelected.id &&
       setProductSelected(EMPTY_PRODUCT);
     titleEditRef.current.focus();
